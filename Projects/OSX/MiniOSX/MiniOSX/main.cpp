72887b6e721211801fa89e1bf9f39d9c600a3124
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2019-01-06 10:28:17 +0100

    updated xcode project to be a proper cocoa app

--- Projects/OSX/MiniOSX/MiniOSX/main.cpp
+++ Projects/OSX/MiniOSX/MiniOSX/main.cpp
@@ -2,40 +2,29 @@
 //  main.cpp
 //  MiniOSX
 //
-//  Created by Jeppe Nielsen on 03/01/2019.
+//  Created by Jeppe Nielsen on 06/01/2019.
 //  Copyright © 2019 Jeppe Nielsen. All rights reserved.
 //
 
-#include <iostream>
-
 #include "Engine.hpp"
 
-using namespace Mini;
+struct Game : Mini::IState {
 
-struct Game : IState {
     void Initialize() override {
-        std::cout << "Started" <<std::endl;
         device.Input.ButtonDown.Bind(this, &Game::ButtonDown);
-        device.Input.TouchDown.Bind(this, &Game::TouchDown);
-        device.Input.TouchUp.Bind(this, &Game::TouchUp);
     }
     
-    void TouchDown(TouchEvent e) {
-        std::cout << "Touch down : " << e.Position << std::endl;
+    void ButtonDown(Mini::ButtonEvent e) {
+        std::cout << e.Id << std::endl;
+        if (e.Id == "j") device.Exit();
     }
-    
-    void TouchUp(TouchEvent e) {
-        std::cout << "Touch Up : " << e.Position << std::endl;
-    }
-    
-    void ButtonDown(ButtonEvent e) {
-        std::cout << "Button  : " << e.Id << std::endl;
-    }
-    
+
     void Update(float dt) override {
+    
     }
 
     void Render() override {
+    
     }
 };
 
