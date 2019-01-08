49ff960a93acb4f088b66f879f7b2ffae84867d8
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2019-01-08 20:00:37 +0100

    rendering works

--- Projects/OSX/MiniOSX/MiniOSX/main.cpp
+++ Projects/OSX/MiniOSX/MiniOSX/main.cpp
@@ -7,12 +7,36 @@
 //
 
 #include "Engine.hpp"
+#include "Scene.hpp"
+#include "RenderSystem.hpp"
 
 using namespace Mini;
+using namespace Mini::ECS;
 
 struct Game : IState {
 
+    Database database;
+    Scene scene;
+    RenderSystem* renderSystem;
+    
+    Game() : scene(database) { }
+
     void Initialize() override {
+    
+        renderSystem = &scene.CreateSystem<RenderSystem>();
+        
+        auto camera = scene.CreateObject();
+        camera.AddComponent<Camera>();
+        camera.AddComponent<Transform>();
+
+        auto cube = scene.CreateObject();
+        cube.AddComponent<Transform>()->Position = {0,0,-10};
+        cube.AddComponent<Mesh>()->GetMesh<Vertex>().AddCube(0, 1);
+        cube.GetComponent<Mesh>()->GetMesh<Vertex>().AddCube({2,0,0}, 1);
+        
+        cube.AddComponent<Renderable>();
+        
+
         device.Input.ButtonDown.Bind(this, &Game::ButtonDown);
         device.ScreenSize.Changed.Bind(this, &Game::ScreenSizeChanged);
     }
@@ -27,11 +51,11 @@ struct Game : IState {
     }
 
     void Update(float dt) override {
-    
+        scene.Update(dt);
     }
 
     void Render() override {
-    
+        renderSystem->Render();
     }
 };
 
