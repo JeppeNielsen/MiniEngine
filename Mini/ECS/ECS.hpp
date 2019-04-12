4d3eade01f0664cbd1bc1729013244fc400d838b
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2019-04-12 20:59:55 +0200

    Added ECS header which all ecs related classes should implement

---
+++ Mini/ECS/ECS.hpp
@@ -0,0 +1,11 @@
+//
+//  ECS.hpp
+//  MiniOSX
+//
+//  Created by Jeppe Nielsen on 11/04/2019.
+//  Copyright © 2019 Jeppe Nielsen. All rights reserved.
+//
+
+#pragma once
+#include "Scene.hpp"
+#include "Hierarchy.hpp"
