66f5be6c76b711b26a6bdab48d341f58f52a735b
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2019-08-11 22:15:55 +0200

    added some more tests to AssetDatabase

--- Projects/OSX/MiniTests/MiniTests/Tests/ECS/TestAssetDatabase.cpp
+++ Projects/OSX/MiniTests/MiniTests/Tests/ECS/TestAssetDatabase.cpp
@@ -9,6 +9,8 @@
 #include "TestAssetDatabase.hpp"
 #include "AssetDatabase.hpp"
 #include "FileHelper.hpp"
+#include <chrono>
+#include <thread>
 
 using namespace Mini;
 using namespace Tests::ECS;
@@ -24,46 +26,93 @@ struct Position {
     TYPE_FIELDS_END
 };
 
-const std::string assetPath = "/Projects/MiniEngine/Projects/OSX/MiniTests/MiniTests/Tests/Assets";
+const std::string assetsPath = "/Projects/MiniEngine/Projects/OSX/MiniTests/MiniTests/Tests/Assets";
 
 void TestAssetDatabase::Run() {
     
     RunTest("Asset database ctor",[]() {
         
         Database database;
-        AssetDatabase assetDatabase(assetPath, database);
+        AssetDatabase assetDatabase(assetsPath, database);
         
         return true;
     });
     
-    /*
+    
     RunTest("Create asset",[]() {
         
         Database database;
-        AssetDatabase assetDatabase(assetPath, database);
+        AssetDatabase assetDatabase(assetsPath, database);
         
         Scene scene(database);
         auto object = scene.CreateObject();
+        object.AddComponent<Position>(123,456);
         
         assetDatabase.CreateAsset(object, "Button.json");
         
-        return FileHelper::FileExists(assetPath+"/" + "Button.json");
+        return assetDatabase.AssetExists("Button.json");
     });
-    */
+    
+    RunTest("Recreate asset, guid remains unchanged",[]() {
+        
+        Database database;
+        AssetDatabase assetDatabase(assetsPath, database);
+        
+        auto guidBefore = assetDatabase.PathToGuid("Button.json");
+        
+        Scene scene(database);
+        auto object = scene.CreateObject();
+        object.AddComponent<Position>(123,456);
+        
+        assetDatabase.CreateAsset(object, "Button.json");
+        
+        auto guidAfter = assetDatabase.PathToGuid("Button.json");
+        
+        return guidBefore == guidAfter;
+    });
     
     RunTest("LoadAsset",[]() {
         
         Database database;
         database.AssureComponent<Position>();
-        AssetDatabase assetDatabase(assetPath, database);
+        AssetDatabase assetDatabase(assetsPath, database);
         
-        auto gameObject = assetDatabase.LoadAsset("Mj0OtpKUTrC65nMgYPriHg==");
+        std::string guid = assetDatabase.PathToGuid("Button.json");
+        
+        auto gameObject = assetDatabase.LoadAsset(guid);
         
         auto pos = gameObject.GetComponent<Position>();
         
-        std::cout << pos->x << std::endl;
-        std::cout << pos->y << std::endl;
-        
-        return (bool)gameObject;
+        return (bool)gameObject && pos->x == 123 && pos->y == 456;
     });
+    
+    
+    /*
+    RunTest("Rename file, guid stays",[]() {
+        
+        using namespace std::chrono_literals;
+        
+        bool hasChanged = false;
+        
+        Database database;
+        database.AssureComponent<Position>();
+        AssetDatabase assetDatabase(assetsPath, database);
+        assetDatabase.Changed.Bind([&](){ hasChanged = true; });
+        
+        
+        std::string guid = assetDatabase.PathToGuid("Button.json");
+        
+        std::this_thread::sleep_for(5s);
+        
+        FileHelper::RenameFile(assetsPath+"/Button.json", assetsPath+"/ButtonNew.json");
+        
+        
+        while (!hasChanged) {
+            std::this_thread::sleep_for(1ms);
+        }
+        
+        std::string pathNew = assetDatabase.GuidToFullPath(guid);
+        return pathNew == (assetsPath + "/ButtonNew.json");
+    });
+    */
 }
